c36405fd2712ad4f56c5aff8e1741d516207ff50
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-12-12 09:33:21 -0700

    adding view content

--- spec/features/user_can_locate_stations_spec.rb
+++ spec/features/user_can_locate_stations_spec.rb
@@ -9,9 +9,8 @@ feature "User can visit root page" do
     click_button("Locate")
 
     expect(current_path).to eq(search_path)
-    # And I fill in the search form with 80203 (Note: Use the existing search form)
-    # And I click "Locate"
-    # Then I should be on page "/search"
+
+    expect(page)
 
     # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
     # And the stations should be limited to Electric and Propane
